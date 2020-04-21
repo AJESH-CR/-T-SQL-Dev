e9518873f697fdee1567aa8fe988953571dc2cbd
AJESH-CR <ajoajesh1996@gmail.com>
2020-04-21 14:38:53 +0530

    Customer table added

--- CreateCabsDb.sql
+++ CreateCabsDb.sql
@@ -1,9 +1,21 @@
+
+--Drop the Database CallUpCabs if exist.
+DROP DATABASE IF EXISTS [CallUpCabs]
+GO
+
+--Create the database CallUpCabs.
 CREATE DATABASE [CallUpCabs]
 GO
 
+--Use the this database for the process.
 USE [CallUpCabs]
 GO
 
+--Drop the cabs table if exist
+DROP TABLE IF EXISTS [dbo].[cab]
+GO
+
+--Creating cabs table
 CREATE TABLE [dbo].[cab](
 	  cabid INT
 	, cabnumber VARCHAR(20)
@@ -11,6 +23,15 @@ CREATE TABLE [dbo].[cab](
 	, fare DECIMAL 
 );
 
+--Creating customer table
+CREATE TABLE [dbo].[customer](
+	 c_id VARCHAR(10) NOT NULL PRIMARY KEY
+	,c_name VARCHAR(30) NOT NULL
+	,c_email VARCHAR(30) NOT NULL
+	,c_address NTEXT NOT NULL
+);
+
+--Adding values to the cab table
 INSERT	INTO [dbo].[cab](cabid, cabnumber, ownername, fare) VALUES(
 	  121
 	 ,'KL-01-SD12'
@@ -36,14 +57,7 @@ INSERT	INTO [dbo].[cab](cabid, cabnumber, ownername, fare) VALUES(
 	 ,10
 );
 
-CREATE TABLE [dbo].[customer](
-	 c_id VARCHAR(10) NOT NULL PRIMARY KEY
-	,c_name VARCHAR(30) NOT NULL
-	,c_email VARCHAR(30) NOT NULL
-	,c_address NTEXT NOT NULL
-	,
-);
-
+--Adding values to the customer table
 INSERT INTO [dbo].[customer](c_id, c_name, c_email, c_address) VALUES(
 	 8289886789
 	,'Ajesh CR'
@@ -64,12 +78,3 @@ INSERT INTO [dbo].[customer](c_id, c_name, c_email, c_address) VALUES(
 );
 GO
 
-CREATE PROCEDURE dbo.GetAllCabNumber
-AS
-BEGIN;
-
-	SELECT [dbo].[cab].[cabnumber] FROM [dbo].[cab];
-
-END;
-
-EXEC dbo.GetAllCabNumber
